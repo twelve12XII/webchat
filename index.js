003f4b2148e2befc56d7a085d7f2d2983b0041a7
twelve12XII <irishka.nikitina2002@mail.ru>
2024-08-04 14:43:35 +0300

    add header

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+app.listen(port);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        comfortaa: ["Comfortaa", "sans-serif"],
+        caveat: ["Caveat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
